da2afa2a7453918f545f299273f516c15d27b6e5
saiumr <colaloveanan@gmail.com>
2023-06-06 15:03:11 +0800

    CH7 finished.

--- test/t_vector3.cpp
+++ test/t_vector3.cpp
@@ -3,6 +3,13 @@
 #include "vector3.hpp"
 
 TEST_CASE("Test Vector3 Class") {
-  math::Vector3 v(0, 0, 0);
-  REQUIRE( v == math::Vector3::kZeroVector );
+  math::Vector3 v0(0, 0, 0);
+  math::Vector3 v1(1, 1, 1);
+  math::Vector3 v2(0, 0, 0);
+  math::Vector3 v3(2, 2, 2);
+  REQUIRE( v0 == math::Vector3::kZeroVector );
+  v2 = v1;
+  REQUIRE( v2 == v1 );
+  REQUIRE( 2*v1 == v3 );
+  REQUIRE( v1*2 == v3 );
 }
